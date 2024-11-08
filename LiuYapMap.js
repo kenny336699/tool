488a54bdcf7c2590773f59133b9ae3ff4b63f5f9
Kenny.Tse <kenny.tse@varmeego.com>
2024-11-08 16:18:34 +0800

    123

--- LiuYapMap.js
+++ LiuYapMap.js
@@ -2,13 +2,13 @@ const trigrams = {
   // 乾(天)
   111: { name: "乾", nature: "1", symbol: "天", number: 1 },
   // 兌(澤)
-  110: { name: "兌", nature: "1", symbol: "澤", number: 2 },
+  "011": { name: "兌", nature: "1", symbol: "澤", number: 2 },
   // 離(火)
   101: { name: "離", nature: "4", symbol: "火", number: 3 },
   // 震(雷)
   "001": { name: "震", nature: "2", symbol: "雷", number: 4 },
   // 巽(風)
-  "011": { name: "巽", nature: "2", symbol: "風", number: 5 },
+  110: { name: "巽", nature: "2", symbol: "風", number: 5 },
   // 坎(水)
   "010": { name: "坎", nature: "3", symbol: "水", number: 6 },
   // 艮(山)
@@ -26,52 +26,49 @@ const hexagrams = {
   // 乾宮卦
   111111: { name: "乾為天", number: 1, palace: 1, type: 1 },
   111110: { name: "姤", number: 44, palace: 1, type: 2 },
-  111100: { name: "遯", number: 33, palace: 1, type: 3 },
+  111100: { name: "遯", number: 14, palace: 1, type: 3 },
   111000: { name: "否", number: 12, palace: 1, type: 4 },
   110000: { name: "觀", number: 20, palace: 1, type: 5 },
   100000: { name: "剝", number: 23, palace: 1, type: 6 },
   101000: { name: "晉", number: 35, palace: 1, type: 7 },
   101111: { name: "大有", number: 14, palace: 1, type: 8 },
 
-  // 坤宮卦
-  "000000": { name: "坤為地", number: 2, palace: 8, type: 1 },
-  "000001": { name: "復", number: 24, palace: 8, type: 2 },
-  "000011": { name: "臨", number: 19, palace: 8, type: 3 },
-  "000111": { name: "泰", number: 11, palace: 8, type: 4 },
-  "001111": { name: "大壯", number: 34, palace: 8, type: 5 },
-  "011111": { name: "夬", number: 43, palace: 8, type: 6 },
-  "010111": { name: "咸", number: 31, palace: 8, type: 7 },
-  "010000": { name: "比", number: 8, palace: 8, type: 8 },
+  110110: { name: "兌為澤", number: 58, palace: 2, type: 1 },
+  110111: { name: "履", number: 10, palace: 2, type: 2 },
+  110101: { name: "大過", number: 28, palace: 2, type: 3 },
+  110001: { name: "困", number: 47, palace: 2, type: 4 },
+  111001: { name: "咸", number: 31, palace: 2, type: 5 },
+  101001: { name: "萃", number: 45, palace: 2, type: 6 },
+  100001: { name: "臨", number: 19, palace: 2, type: 7 },
+  100110: { name: "損", number: 41, palace: 2, type: 8 },
 
-  // 震宮卦
+  101101: { name: "離為火", number: 30, palace: 3, type: 1 },
+  101100: { name: "旅", number: 56, palace: 3, type: 2 },
+  101110: { name: "鼎", number: 50, palace: 3, type: 3 }, // 修改编号为50
+  101010: { name: "未濟", number: 64, palace: 3, type: 4 }, // 修改编号为64
+  100010: { name: "蒙", number: 4, palace: 3, type: 5 }, // 修改编号为4
+  110010: { name: "渙", number: 59, palace: 3, type: 6 }, // 修改编号为59
+  111010: { name: "訟", number: 6, palace: 3, type: 7 }, // 修改编号为6
+  111101: { name: "同人", number: 13, palace: 3, type: 8 }, // 修改编号为13
+
   "001001": { name: "震為雷", number: 51, palace: 4, type: 1 },
   "001000": { name: "豫", number: 16, palace: 4, type: 2 },
   "001010": { name: "解", number: 40, palace: 4, type: 3 },
   "001110": { name: "恆", number: 32, palace: 4, type: 4 },
   "000110": { name: "升", number: 46, palace: 4, type: 5 },
   "010110": { name: "井", number: 48, palace: 4, type: 6 },
-  "011110": { name: "升", number: 46, palace: 4, type: 7 },
-  "011001": { name: "恆", number: 32, palace: 4, type: 8 },
+  "011010": { name: "大過", number: 28, palace: 4, type: 7 },
+  "011001": { name: "隨", number: 17, palace: 4, type: 8 },
 
-  // 離宮卦
-  101101: { name: "離為火", number: 30, palace: 3, type: 1 },
-  101100: { name: "旅", number: 56, palace: 3, type: 2 },
-  101110: { name: "大過", number: 28, palace: 3, type: 3 },
-  101010: { name: "鼎", number: 50, palace: 3, type: 4 },
-  100010: { name: "升", number: 46, palace: 3, type: 5 },
-  110010: { name: "小過", number: 62, palace: 3, type: 6 },
-  111010: { name: "鼎", number: 50, palace: 3, type: 7 },
-  111101: { name: "離", number: 30, palace: 3, type: 8 },
-
-  // 艮宮卦
-  100100: { name: "艮為山", number: 52, palace: 7, type: 1 },
-  100101: { name: "蠱", number: 18, palace: 7, type: 2 },
-  100111: { name: "賁", number: 22, palace: 7, type: 3 },
-  100011: { name: "明夷", number: 36, palace: 7, type: 4 },
-  101011: { name: "師", number: 7, palace: 7, type: 5 },
-  111011: { name: "小畜", number: 9, palace: 7, type: 6 },
-  110011: { name: "遁", number: 33, palace: 7, type: 7 },
-  110100: { name: "謙", number: 15, palace: 7, type: 8 },
+  // 巽宮卦
+  "011011": { name: "巽為風", number: 57, palace: 5, type: 1 },
+  "011010": { name: "小畜", number: 9, palace: 5, type: 2 },
+  "011000": { name: "家人", number: 37, palace: 5, type: 3 },
+  "011100": { name: "益", number: 42, palace: 5, type: 4 },
+  "010100": { name: "無妄", number: 25, palace: 5, type: 5 },
+  "000100": { name: "噬嗑", number: 21, palace: 5, type: 6 },
+  "001100": { name: "頤", number: 27, palace: 5, type: 7 },
+  "001011": { name: "蠱", number: 18, palace: 5, type: 8 },
 
   // 坎宮卦
   "010010": { name: "坎為水", number: 29, palace: 6, type: 1 },
@@ -83,33 +80,33 @@ const hexagrams = {
   "000101": { name: "明夷", number: 36, palace: 6, type: 7 },
   "000010": { name: "師", number: 7, palace: 6, type: 8 },
 
-  // 巽宮卦
-  "011011": { name: "巽為風", number: 57, palace: 5, type: 1 },
-  "011010": { name: "漸", number: 53, palace: 5, type: 2 },
-  "011000": { name: "觀", number: 20, palace: 5, type: 3 },
-  "011100": { name: "中孚", number: 61, palace: 5, type: 4 },
-  "010100": { name: "家人", number: 37, palace: 5, type: 5 },
-  "000100": { name: "益", number: 42, palace: 5, type: 6 },
-  "001100": { name: "渙", number: 59, palace: 5, type: 7 },
-  "001011": { name: "隨", number: 17, palace: 5, type: 8 },
+  // 艮宮卦
+  100100: { name: "艮為山", number: 52, palace: 7, type: 1 },
+  100101: { name: "山火賁", number: 22, palace: 7, type: 2 },
+  100111: { name: "山天大畜", number: 26, palace: 7, type: 3 },
+  100011: { name: "山澤損", number: 41, palace: 7, type: 4 },
+  101011: { name: "火澤睽", number: 38, palace: 7, type: 5 },
+  111011: { name: "天澤履", number: 10, palace: 7, type: 6 },
+  110011: { name: "風澤中孚", number: 61, palace: 7, type: 7 },
+  110100: { name: "風山漸", number: 53, palace: 7, type: 8 },
 
-  // 兌宮卦
-  110110: { name: "兌為澤", number: 58, palace: 2, type: 1 },
-  110111: { name: "隨", number: 17, palace: 2, type: 2 },
-  110101: { name: "大過", number: 28, palace: 2, type: 3 },
-  110001: { name: "困", number: 47, palace: 2, type: 4 },
-  111001: { name: "咸", number: 31, palace: 2, type: 5 },
-  101001: { name: "萃", number: 45, palace: 2, type: 6 },
-  100001: { name: "臨", number: 19, palace: 2, type: 7 },
-  100110: { name: "損", number: 41, palace: 2, type: 8 },
+  // 坤宮卦
+  "000000": { name: "坤為地", number: 2, palace: 8, type: 1 },
+  "000001": { name: "復", number: 24, palace: 8, type: 2 },
+  "000011": { name: "臨", number: 19, palace: 8, type: 3 },
+  "000111": { name: "泰", number: 11, palace: 8, type: 4 },
+  "001111": { name: "大壯", number: 34, palace: 8, type: 5 },
+  "011111": { name: "夬", number: 43, palace: 8, type: 6 },
+  "010111": { name: "咸", number: 31, palace: 8, type: 7 },
+  "010000": { name: "比", number: 8, palace: 8, type: 8 },
 };
 
 // Alternative array format
 const BRANCH_ELEMENT_ARRAY = [
-  { branch: "子", element: "水", number: 2 },
+  { branch: "子", element: "水", number: 3 },
   { branch: "丑", element: "土", number: 5 },
-  { branch: "寅", element: "木", number: 3 },
-  { branch: "卯", element: "木", number: 3 },
+  { branch: "寅", element: "木", number: 2 },
+  { branch: "卯", element: "木", number: 2 },
   { branch: "辰", element: "土", number: 5 },
   { branch: "巳", element: "火", number: 4 },
   { branch: "午", element: "火", number: 4 },
@@ -117,7 +114,7 @@ const BRANCH_ELEMENT_ARRAY = [
   { branch: "申", element: "金", number: 1 },
   { branch: "酉", element: "金", number: 1 },
   { branch: "戌", element: "土", number: 5 },
-  { branch: "亥", element: "水", number: 2 },
+  { branch: "亥", element: "水", number: 3 },
 ];
 
 // 地支列表
@@ -142,22 +139,22 @@ const PALACE_NAJIA_RULES = {
     // 兌宮：兌內丁巳外丁亥
     inner: {
       stem: "丁",
-      branches: [branchMap["巳"], branchMap["未"], branchMap["酉"]],
+      branches: [branchMap["巳"], branchMap["卯"], branchMap["丑"]],
     },
     outer: {
       stem: "丁",
-      branches: [branchMap["亥"], branchMap["丑"], branchMap["卯"]],
+      branches: [branchMap["亥"], branchMap["酉"], branchMap["未"]],
     },
   },
   3: {
     // 離宮：離內己卯外己酉
     inner: {
       stem: "己",
-      branches: [branchMap["卯"], branchMap["巳"], branchMap["未"]],
+      branches: [branchMap["卯"], branchMap["丑"], branchMap["亥"]],
     },
     outer: {
       stem: "己",
-      branches: [branchMap["酉"], branchMap["亥"], branchMap["丑"]],
+      branches: [branchMap["酉"], branchMap["未"], branchMap["己"]],
     },
   },
   4: {
@@ -175,11 +172,11 @@ const PALACE_NAJIA_RULES = {
     // 巽宮：巽內辛丑外辛未
     inner: {
       stem: "辛",
-      branches: [branchMap["丑"], branchMap["卯"], branchMap["巳"]],
+      branches: [branchMap["丑"], branchMap["亥"], branchMap["酉"]],
     },
     outer: {
       stem: "辛",
-      branches: [branchMap["未"], branchMap["酉"], branchMap["亥"]],
+      branches: [branchMap["未"], branchMap["已"], branchMap["卯"]],
     },
   },
   6: {
@@ -208,11 +205,11 @@ const PALACE_NAJIA_RULES = {
     // 坤宮：坤內乙未外癸丑
     inner: {
       stem: "乙",
-      branches: [branchMap["未"], branchMap["酉"], branchMap["亥"]],
+      branches: [branchMap["未"], branchMap["巳"], branchMap["卯"]],
     },
     outer: {
       stem: "癸",
-      branches: [branchMap["丑"], branchMap["卯"], branchMap["巳"]],
+      branches: [branchMap["丑"], branchMap["亥"], branchMap["亥"]],
     },
   },
 };
@@ -220,36 +217,36 @@ const PALACE_NAJIA_RULES = {
 const FIVE_PHASE_RELATIONS = {
   1: {
     1: "兄弟", //金
-    2: "官鬼", //木
+    2: "妻財", //木
     3: "子孫", //水
-    4: "妻財", //火
+    4: "官鬼", //火
     5: "父母", //土
   },
   2: {
-    1: "妻財",
+    1: "官鬼",
     2: "兄弟",
     3: "父母",
     4: "子孫",
-    5: "官鬼",
+    5: "妻財",
   },
   3: {
     1: "父母",
-    2: "妻財",
+    2: "子孫",
     3: "兄弟",
-    4: "官鬼",
-    5: "子孫",
+    4: "妻財",
+    5: "官鬼",
   },
   4: {
-    1: "官鬼",
+    1: "妻財",
     2: "父母",
-    3: "妻財",
+    3: "官鬼",
     4: "兄弟",
     5: "子孫",
   },
   5: {
     1: "子孫",
-    2: "妻財",
-    3: "官鬼",
+    2: "官鬼",
+    3: "妻財",
     4: "父母",
     5: "兄弟",
   },
@@ -267,7 +264,10 @@ function getHexagramNajia(hexagramKey) {
   if (hexStr.length !== 6) {
     throw new Error("Hexagram key must be 6 digits long");
   }
-
+  console.log(
+    "getTrigramByNumber(hexagrams[hexagramKey].palace).nature",
+    getTrigramByNumber(hexagrams[hexagramKey].palace).nature
+  );
   const hexgramEle =
     FIVE_PHASE_RELATIONS[
       getTrigramByNumber(hexagrams[hexagramKey].palace).nature
@@ -286,24 +286,26 @@ function getHexagramNajia(hexagramKey) {
 
   //找世應
   let self = 6;
+
+  console.log("hexagrams[hexagramKey].type", hexagrams[hexagramKey].type);
   switch (hexagrams[hexagramKey].type) {
     case 1:
-      self = 1;
+      self = 6;
       break;
     case 2:
-      self = 2;
+      self = 1;
       break;
     case 3:
       self = 2;
       break;
     case 4:
-      self = 4;
+      self = 3;
       break;
     case 5:
-      self = 5;
+      self = 4;
       break;
     case 6:
-      self = 6;
+      self = 5;
       break;
     case 7:
       self = 4;
@@ -313,10 +315,10 @@ function getHexagramNajia(hexagramKey) {
       break;
   }
   const resp = self >= 4 ? self - 3 : self + 3;
-
+  //安六親
   let liuSiu = [];
   if (naJiaInner && Array.isArray(naJiaInner.branches)) {
-    console.log("1");
+    console.log("naJiaInner.branches[i].number", naJiaInner.branches[0].number);
     for (let i = 0; i < naJiaInner.branches.length; i++) {
       liuSiu.push(hexgramEle[naJiaInner.branches[i].number]);
     }
@@ -327,6 +329,56 @@ function getHexagramNajia(hexagramKey) {
     }
   }
   liuSiu = liuSiu.reverse();
+
+  //安伏神, 找出本卦所缺的六親, 然後從卦的本宮找出對應的六親及其位置
+  // Calculate hidden spirits (伏神)
+  const allRelations = new Set(["兄弟", "子孫", "妻財", "官鬼", "父母"]);
+  const currentRelations = new Set(liuSiu);
+  const missingRelations = [...allRelations].filter(
+    (x) => !currentRelations.has(x)
+  );
+
+  // Get palace hexagram key
+  const palaceNumber = hexagrams[hexagramKey].palace;
+  const palaceHexagramKey = Object.keys(hexagrams).find(
+    (key) => hexagrams[key].palace === palaceNumber && hexagrams[key].type === 1
+  );
+
+  // Get palace hexagram's six relations
+  const palaceNajia = {
+    hexgramEle,
+    outerTrigramKey: palaceHexagramKey.slice(0, 3),
+    innerTrigramKey: palaceHexagramKey.slice(3),
+  };
+
+  const palaceOuterTrigram = trigrams[palaceNajia.outerTrigramKey];
+  const palaceInnerTrigram = trigrams[palaceNajia.innerTrigramKey];
+
+  const palaceNaJiaInner = PALACE_NAJIA_RULES[palaceInnerTrigram.number].inner;
+  const palaceNaJiaOuter = PALACE_NAJIA_RULES[palaceOuterTrigram.number].outer;
+
+  let palaceLiuSiu = [];
+  if (palaceNaJiaInner && Array.isArray(palaceNaJiaInner.branches)) {
+    for (let i = 0; i < palaceNaJiaInner.branches.length; i++) {
+      palaceLiuSiu.push(hexgramEle[palaceNaJiaInner.branches[i].number]);
+    }
+  }
+  if (palaceNaJiaOuter && Array.isArray(palaceNaJiaOuter.branches)) {
+    for (let i = 0; i < palaceNaJiaOuter.branches.length; i++) {
+      palaceLiuSiu.push(hexgramEle[palaceNaJiaOuter.branches[i].number]);
+    }
+  }
+
+  // Map missing relations to their positions in palace hexagram
+  const hiddenSpirits = missingRelations.map((relation) => ({
+    relation,
+    positions: palaceLiuSiu.reduce((acc, curr, idx) => {
+      if (curr === relation) acc.push(idx + 1);
+      return acc;
+    }, []),
+  }));
+  console.log(hiddenSpirits);
+
   return {
     hexgramEle,
     outerTrigramKey,
@@ -334,10 +386,13 @@ function getHexagramNajia(hexagramKey) {
     outerTrigram,
     innerTrigram,
     naJiaOuter,
+    1: naJiaOuter.branches,
     naJiaInner,
+    2: naJiaInner.branches,
     liuSiu,
     self,
     resp,
+    hiddenSpirits,
   };
 }
 
@@ -345,5 +400,5 @@ function getHexagramNajia(hexagramKey) {
 function getStemBranchCombination(stem, branch) {
   return `${stem}${branch}`;
 }
-const najiaInfo = getHexagramNajia("100111"); // 取得乾卦納甲信息
+const najiaInfo = getHexagramNajia("011101"); // 取得乾卦納甲信息
 console.log(najiaInfo);
